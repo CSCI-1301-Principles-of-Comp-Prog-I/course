c8fb9a71def2b5cac205d3e6c63b22d1718b5697
Harley Eades <harley.eades@gmail.com>
2020-01-21 15:44:28 -0500

    Adding today's lecture notes.

---
+++ 01212020.py
@@ -0,0 +1,26 @@
+import math
+
+str1 = "Hello"
+str2 = "World"
+
+# Pythagorian Theorem
+a0 = 2
+b0 = 6
+c0 = (a0**2 + b0**2) ** (1/2)
+
+a1 = 5
+b1 = 13
+c1 = (a1**2 + b1**2) ** (1/2)
+
+sum = c0 + c1
+
+# Area of a circle:
+radius = 24
+area = radius**2 * math.pi
+
+# Quad. formula
+a = 0
+b = 0
+c = 0
+xPlus = ((-b) + math.sqrt(b**2 - 4 * a * c)) / 2 * a
+xMinus = ((-b) - math.sqrt(b**2 - 4 * a * c)) / 2 * a
---
+++ grade_report.py
@@ -0,0 +1,22 @@
+# Grade Report
+
+hwk0 = 0
+hwk1 = 0
+hwk2 = 0
+hwk3 = 0
+hwk4 = 0
+hwk5 = 0
+hwk6 = 0
+hwk7 = 0
+
+quiz0 = 0
+
+sum = hwk0 + hwk1 + hwk2 + hwk3 + hwk4 + hwk5 + hwk6 + hwk7 + quiz0
+totalAssignments = 9
+
+grade = sum / totalAssignments
+
+print ("My current grade is "+str(grade))
+
+
+
